77f430cc4673170c421b9391522f86bb184dda9c
DanielKMach <danikoucher@gmail.com>
2022-09-15 22:55:55 -0300

    Changed spaces to tabs

--- main.ts
+++ main.ts
@@ -4,168 +4,168 @@
 //% color=#009b5b icon="\uf1eb" block="ESP Wi-Fi"
 namespace espwifi {
 
-    const CRLF = "\r\n"
+	const CRLF = "\r\n"
 
-    let response = ""
-    let request_response = ""
-    let request_successful = false
+	let response = ""
+	let request_response = ""
+	let request_successful = false
 
-    /**
-     * Initializes serial conection to ESP
-     */
-    //% block="initialize ESP|TX: %tx|RX: %rx|baud rate: %baudrate"
-    //% tx.defl=SerialPin.P0 rx.defl=SerialPin.P1 baudrate.defl=115200
-    export function initialize(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
-        serial.redirect(tx, rx, baudrate)
-        basic.pause(1000)
-    }
+	/**
+	 * Initializes serial conection to ESP
+	 */
+	//% block="initialize ESP|TX: %tx|RX: %rx|baud rate: %baudrate"
+	//% tx.defl=SerialPin.P0 rx.defl=SerialPin.P1 baudrate.defl=115200
+	export function initialize(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+		serial.redirect(tx, rx, baudrate)
+		basic.pause(1000)
+	}
 
-    /**
-     * Connects to the wifi with the given name and password
-     */
-    //% block="connect to the Wi-Fi %name with the password %password"
-    //% name.defl="wifi name" password.defl="wifi password"
-    export function connectWifi(name: string, password: string) {
-        sendAT("CWMODE=1") // set to station mode
-        basic.pause(500)
-        sendAT(`CWJAP="${name}","${password}"`)
-    }
+	/**
+	 * Connects to the wifi with the given name and password
+	 */
+	//% block="connect to the Wi-Fi %name with the password %password"
+	//% name.defl="wifi name" password.defl="wifi password"
+	export function connectWifi(name: string, password: string) {
+		sendAT("CWMODE=1") // set to station mode
+		basic.pause(500)
+		sendAT(`CWJAP="${name}","${password}"`)
+	}
 
-    //% block="Disconnect from the Wi-Fi"
-    export function disconnectWifi() {
-        sendAT("CWQAP")
-    }
+	//% block="disconnect from the Wi-Fi"
+	export function disconnectWifi() {
+		sendAT("CWQAP")
+	}
 
-    /**
-     * Make a GET request with the ESP
-     */
-    //% block="make request to %ip %args"
-    //% ip.defl="api.thingspeak.com" args.defl="/update?api_key=[your_key]%field0=69"
-    export function request(ip: string, args: string) {
+	/**
+	 * Make a GET request with the ESP
+	 */
+	//% block="make request to %ip %args"
+	//% ip.defl="api.thingspeak.com" args.defl="/update?api_key=[your_key]%field0=69"
+	export function request(ip: string, args: string) {
 
-        // Start connection to the IP
-        let connected_to_server = sendAT(`CIPSTART="TCP","${ip}",80`)
+		// Start connection to the IP
+		let connected_to_server = sendAT(`CIPSTART="TCP","${ip}",80`)
 
-        if (!connected_to_server)
-            return
+		if (!connected_to_server)
+			return
 
-        // Build and send request
-        const request = "GET " + args + CRLF
-        sendAT("CIPSEND=" + request.length) // Define requets length
-        basic.pause(500)
+		// Build and send request
+		const request = "GET " + args + CRLF
+		sendAT("CIPSEND=" + request.length) // Define requets length
+		basic.pause(500)
 
-        serial.writeString(request) // Send request
-        waitResponse()
+		serial.writeString(request) // Send request
+		waitResponse()
 
-        request_response = response.trim()
-        request_successful = request_response.includes("SEND OK")
+		request_response = response.trim()
+		request_successful = request_response.includes("SEND OK")
 
-        sendAT("CIPCLOSE") // Closes connection
-    }
+		sendAT("CIPCLOSE") // Closes connection
+	}
 
-    // write AT command with CR+LF (\r\n) ending
-    function sendAT(at?: string): boolean {
-        if (at != undefined)
-            serial.writeString("AT+" + at + CRLF)
-        else
-            serial.writeString("AT" + CRLF)
+	// write AT command with CR+LF (\r\n) ending
+	function sendAT(at?: string): boolean {
+		if (at != undefined)
+			serial.writeString("AT+" + at + CRLF)
+		else
+			serial.writeString("AT" + CRLF)
 
-        return waitResponse()
-    }
+		return waitResponse()
+	}
 
-    function waitResponse(): boolean {
-        response = ""
-        const time = input.runningTime()
+	function waitResponse(): boolean {
+		response = ""
+		const time = input.runningTime()
 
-        while (input.runningTime() - time < 30000) { // 30 sec time out
-            response += serial.readString()
-            const lines = response.trim().split(CRLF)
+		while (input.runningTime() - time < 30000) { // 30 sec time out
+			response += serial.readString()
+			const lines = response.trim().split(CRLF)
 
-            for (let line of lines) {
-                line = line.trim()
-                if (line.length == 0) continue
+			for (let line of lines) {
+				line = line.trim()
+				if (line.length == 0) continue
 
-                if (line == "OK" || line == "CLOSED") {
-                    music.playTone(700, 100)
-                    return true
-                }
+				if (line == "OK" || line == "CLOSED") {
+					music.playTone(700, 100)
+					return true
+				}
 
-                if (line == "ERROR") {
-                    music.playTone(200, 100)
-                    return false
-                }
-            }
-        }
-        return false
-    }
+				if (line == "ERROR") {
+					music.playTone(200, 100)
+					return false
+				}
+			}
+		}
+		return false
+	}
 
-    /**
-     * Restore ESP to factory default settings
-     */
-    //% block="restore ESP to factory default"
-    export function restoreFactoryDefault() {
-        sendAT("RESTORE") // restore to factory default settings
-        basic.pause(1000)
-    }
+	/**
+	 * Restore ESP to factory default settings
+	 */
+	//% block="restore ESP to factory default"
+	export function restoreFactoryDefault() {
+		sendAT("RESTORE") // restore to factory default settings
+		basic.pause(1000)
+	}
 
-    /**
-     * Restart ESP
-     */
-    //% block="restart ESP"
-    export function restart() {
-        sendAT("RST")
-        basic.pause(1000)
-    }
+	/**
+	 * Restart ESP
+	 */
+	//% block="restart ESP"
+	export function restart() {
+		sendAT("RST")
+		basic.pause(1000)
+	}
 
-    /**
-     * Retrieves the data form the last request
-     */
-    //% block="data from last request"
-    export function dataFromLastRequest(): string {
-        if (!lastRequestSuccessful()) return ""
-        if (!request_response.includes("+IPD")) return ""
+	/**
+	 * Retrieves the data form the last request
+	 */
+	//% block="data from last request"
+	export function dataFromLastRequest(): string {
+		if (!lastRequestSuccessful()) return ""
+		if (!request_response.includes("+IPD")) return ""
 
-        const start = request_response.indexOf(':') + 1
-        const end = request_response.length - 6
-        let data = request_response.slice(start, end)
-        data = data.trim()
-        return data
-    }
+		const start = request_response.indexOf(':') + 1
+		const end = request_response.length - 6
+		let data = request_response.slice(start, end)
+		data = data.trim()
+		return data
+	}
 
-    /**
-     * Is ESP connected to the Wi-Fi AP?
-     */
-    //% block="ESP connected to Wi-Fi"
-    export function isWifiConnected(): boolean {
-        sendAT("CWJAP?")
-        return response.includes("+CWJAP:")
-    }
+	/**
+	 * Is ESP connected to the Wi-Fi AP?
+	 */
+	//% block="ESP connected to Wi-Fi"
+	export function isWifiConnected(): boolean {
+		sendAT("CWJAP?")
+		return response.includes("+CWJAP:")
+	}
 
-    /**
-     * Returns the name of the ESP's connected Wi-Fi
-     */
-    //% block="ESP connected Wi-Fi name"
-    export function connectedWifiName(): string {
-        if (!isWifiConnected()) return ""
+	/**
+	 * Returns the name of the ESP's connected Wi-Fi
+	 */
+	//% block="ESP connected Wi-Fi name"
+	export function connectedWifiName(): string {
+		if (!isWifiConnected()) return ""
 
-        const start = response.indexOf('"') + 1
-        const end = response.indexOf('"', start)
-        return response.slice(start, end)
-    }
+		const start = response.indexOf('"') + 1
+		const end = response.indexOf('"', start)
+		return response.slice(start, end)
+	}
 
-    /**
-     * Was last request sent successful?
-     */
-    //% block="last request successful"
-    export function lastRequestSuccessful(): boolean {
-        return request_successful
-    }
+	/**
+	 * Was last request sent successful?
+	 */
+	//% block="last request successful"
+	export function lastRequestSuccessful(): boolean {
+		return request_successful
+	}
 
-    /**
-     * Is ESP ready to use?
-     */
-    //% block="ESP ready"
-    export function isReady(): boolean {
-        return sendAT()
-    }
+	/**
+	 * Is ESP ready to use?
+	 */
+	//% block="ESP ready"
+	export function isReady(): boolean {
+		return sendAT()
+	}
 }
